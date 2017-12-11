d11c053275edebcfdd731b3f37526c10123942ec
rafaeldamasceno <rafaeldamasceno@me.com>
2017-12-11 15:50:57 +0000

    Added time measuring and test.

--- TP2/skyscraper.pl
+++ TP2/skyscraper.pl
@@ -28,11 +28,28 @@ getLines([Line | TL], [NumberFront | TF], [NumberBack | TB]) :-
 
 
 skyscraper([Left, Right, Up, Down], Solution) :-
+  statistics(walltime, [T0 | _]),
   same_length(Solution, Left),
   maplist(same_length(Up), Solution),
   append(Solution, SolutionNumbers),  
   getLines(Solution, Left, Right),
   transpose(Solution, TransposedSolution),
   getLines(TransposedSolution, Up, Down),
-  labeling([], SolutionNumbers).
+  statistics(walltime, [T1 | _]),
+  labeling([leftmost,bisect,up], SolutionNumbers),
+  statistics(walltime, [T2 | _]),
+  Time1 is T1 - T0,
+  Time2 is T2 - T1,
+  write('C: '), write(Time1), nl,
+  write('L: '), write(Time2), nl.
   
+test1 :-
+  skyscraper(
+    [
+      [_, 2, 3, 4, _, _], 
+      [_, _, 4, 3, 2, _], 
+      [5, _, _, 2, 2, _], 
+      [_, 3, 4, _, _, 4]
+    ], 
+    Solution),
+    maplist(portray_clause, Solution).
