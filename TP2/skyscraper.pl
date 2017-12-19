31d2e4eda615146a1ac384baa1b3cc1f63b9561d
rafaeldamasceno <rafaeldamasceno@me.com>
2017-12-19 16:23:37 +0000

    Increased efficiency of disjunction method by a lot.

--- TP2/skyscraper.pl
+++ TP2/skyscraper.pl
@@ -4,39 +4,59 @@
 :- include('boardGeneration.pl').
 
 getLine(Line, Number) :-
-  length(Line, Length),
-  domain(Line, 1, Length),
   all_distinct(Line),
-  visualizeSignature(Line, Number).
+  getLineDisjunction(Line, Number, 0).
+  % reverse(Line, LineReversed),
+  % getLineReification(LineReversed, Number).
 
-getLineAux([H | T], Length, Number, Max) :-
+getLineDisjunction([], 0, _).
+
+getLineDisjunction([H | T], Number, Max) :-
   (H #> Max #/\
   NewMax #= H #/\
-  NewNumber #= Number - 1) #\/
+  NewNumber #= Number - 1) #\
   (H #< Max #/\
   NewMax #= Max #/\
   NewNumber #= Number),
-  getLineAux(T, Length, NewNumber, NewMax).
+  getLineDisjunction(T, NewNumber, NewMax).
 
-getLineAux([], Length, 0, Length).
+getLineReification([], 0).
+
+getLineReification([H | T], Value) :-
+  getLineReification(T, NewValue),
+  maximum(A, [H | T]),
+  H #= A #<=> S,
+  Value #= NewValue + S.
+
+waitUntilMax(_, [], _).
+
+waitUntilMax(Size, [H | T], Number) :-
+  H #= Size #<=> (T #= [], getLineReification([H | T], Number)),
+  waitUntilMax(Size, T, Number).
 
 getLines([], [], []).
+
 getLines([Line | TL], [NumberFront | TF], [NumberBack | TB]) :-
-  getLine(Line, NumberBack),
+  getLine(Line, NumberFront),
   reverse(Line, LineReversed),
-  getLine(LineReversed, NumberFront),
+  getLine(LineReversed, NumberBack),
   getLines(TL, TF, TB).
 
 skyscraper([Left, Right, Up, Down], Solution) :-
   statistics(walltime, [T0 | _]),
-  same_length(Solution, Left),
-  maplist(same_length(Up), Solution),
-  append(Solution, SolutionNumbers),  
+  length(Left, Length),
+  length(Right, Length),
+  length(Up, Length),
+  length(Down, Length),
+  length(Solution, Length),
+  maplist(same_length(Solution), Solution),
+  append(Solution, FlatSolution),  
+  domain(FlatSolution, 1, Length),
   getLines(Solution, Left, Right),
   transpose(Solution, TransposedSolution),
   getLines(TransposedSolution, Up, Down),
   statistics(walltime, [T1 | _]),
-  labeling([max, bisect, up], SolutionNumbers),
+  labeling([max, bisect, up], FlatSolution),
   statistics(walltime, [T2 | _]),
   Time1 is T1 - T0,
   Time2 is T2 - T1,
@@ -54,20 +74,19 @@ test1 :-
   Solution),
   maplist(portray_clause, Solution).
 
-test2(Size) :-
+test2 :-
+  skyscraper(
+  [
+    [3, 3, _, 3, _, 3, _, _], 
+    [_, _, 2, _, 4, 4, _, 1],
+    [_, _, 5, 3, _, 2, _, 4],
+    [2, 4, _, _, 4, _, _, _]
+  ], 
+  Solution),
+  maplist(portray_clause, Solution).
+
+generate1(Size) :-
   boardGeneration(Size, Board),
   maplist(portray_clause, Board), nl, nl,
   skyscraper(Board, Solution),
   maplist(portray_clause, Solution).
-
-waitUntilMax(_, [], _).
-waitUntilMax(Size, [H | T], Number) :-
-  H #= Size #<=> (T #= [], visualizeSignature([H | T], Number)),
-  waitUntilMax(Size, T, Number).
-
-visualizeSignature([], 0).
-visualizeSignature([H | T], Value) :-
-  visualizeSignature(T, NewValue),
-  maximum(A, [H | T]),
-  H #= A #<=> S,
-  Value #= NewValue + S.
